db785944602f6f78fb5dfb0a176a57428c0c6c0d
Jack Arthur Harrhy <me@jackharrhy.com>
2019-06-24 10:23:55 -0230

    readtostr -> bufread, colors, clippy

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use std::io::{self, Read};
+use std::env;
+use std::fs;
+use std::io::{self, BufRead, BufReader};
+
 use serde_json::{Map, Value};
 
-fn handle_array(array: &Vec<Value>, mut location: &mut String) {
+use colored::*;
+
+fn handle_array(array: &[Value], mut location: &mut String) {
     location.push('[');
     for (index, item) in array.iter().enumerate() {
-        let new_position = format!("{}]", index);
+        let new_position = format!("{}]", index.to_string().cyan());
         location.push_str(&new_position);
         handle_value(&item, &mut location);
         location.truncate(location.len() - new_position.len());
@@ -29,20 +34,23 @@ fn handle_value(value: &Value, mut location: &mut String) {
     match value {
         Value::Object(object) => handle_object(&object, &mut location),
         Value::Array(array) => handle_array(&array, &mut location),
-        Value::Bool(boolean) => println!("{} = {}", location, boolean),
+        Value::Bool(boolean) => println!("{} = {}", location, boolean.to_string().yellow()),
         Value::Number(number) => println!("{} = {}", location, number),
-        Value::String(string) => println!("{} = \"{}\"", location, string),
+        Value::String(string) => {
+            println!("{} = \"{}\"", location, string.to_string().bright_black())
+        }
         Value::Null => (),
     };
 }
 
 fn main() -> io::Result<()> {
-    let mut buffer = String::new();
-    let stdin = io::stdin();
-    let mut handle = stdin.lock();
-    handle.read_to_string(&mut buffer)?;
+    let input = env::args().nth(1);;
+    let reader: Box<BufRead> = match input {
+        None => Box::new(BufReader::new(io::stdin())),
+        Some(filename) => Box::new(BufReader::new(fs::File::open(filename).unwrap())),
+    };
 
-    let base: Value = serde_json::from_str(&buffer)?;
+    let base: Value = serde_json::from_reader(reader)?;
 
     let mut location = String::new();
     location.push('.');
